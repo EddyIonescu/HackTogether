6414c2ad980da0998db38c7debe6697e49b82346
Eddy Ionescu <eddyionescu@gmail.com>
2016-01-24 13:06:55 -0500

    Update TeamFormation.py
    Fixed for cases where not enough intermediates/gurus

--- TeamFormation.py
+++ TeamFormation.py
@@ -4,7 +4,6 @@
 # then match those to the first advanced that has all the required skills and lowest score,
 # and then match to guru with all the required skills and lowest score
 
-#Python 2.7.3
 import json,httplib
 
 #Get user-list
@@ -56,30 +55,29 @@ for noob in newbies:
             maxindex = index
         index += 1
     team = []
-    team.append(noob)
     if maxindex>-1:
+        team.append(noob)
         team.append(inters[maxindex])
         inters.pop(maxindex)
-    else:
-        team.append({"username": ""})
-    teams.append(team)
+        #otherwise, this newbie won't be matched
+        teams.append(team)
 for t in teams:
     maxclose = 0
     maxindex = -1
     index = 0
     score = 0
     for inter in inters:
-        if team[1]["ios"] and inter["ios"]:
+        if t[1]["ios"] and inter["ios"]:
             score += 1
         else:
             score -= 0.5
-        if team[1]["android"] and inter["android"]:
+        if t[1]["android"] and inter["android"]:
             score += 1
         else:
             score -= 0.5
-        if team[1]["web"] and inter["web"]:
+        if t[1]["web"] and inter["web"]:
             score += 1
-        if team[1]["hardware"] and inter["hardware"]:
+        if t[1]["hardware"] and inter["hardware"]:
             score += 1
         if score>maxclose:
             maxclose = score
@@ -96,17 +94,17 @@ for t in teams:
     index = 0
     score = 0
     for guru in gurus:
-        if team[0]["ios"] and guru["ios"]:
+        if t[2]["ios"] and guru["ios"]:
             score += 1
         else:
             score -= 0.5
-        if team[0]["android"] and guru["android"]:
+        if t[2]["android"] and guru["android"]:
             score += 1
         else:
             score -= 0.5
-        if team[0]["web"] and guru["web"]:
+        if t[2]["web"] and guru["web"]:
             score += 1
-        if team[0]["hardware"] and guru["hardware"]:
+        if t[2]["hardware"] and guru["hardware"]:
             score += 1
         if score>maxclose:
             maxclose = score
